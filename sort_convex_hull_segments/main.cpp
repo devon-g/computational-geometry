c633ce8400daedd0eb12cf1e375dbc68d2a55e93
Devon Gardner <dgardner365@gmail.com>
2022-10-04 15:57:39 -0400

    basic template for convex hull segment sorting algorithm

---
+++ sort_convex_hull_segments/main.cpp
@@ -0,0 +1,11 @@
+#include <CGAL/Simple_cartesian.h>
+
+typedef CGAL::Simple_cartesian<double> Kernel;
+typedef Kernel::Point_2 Point_2;
+typedef Kernel::Segment_2 Segment_2;
+
+
+
+int main() {
+
+}
